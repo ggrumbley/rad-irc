d0d7e50f99c6e7af58605e7c36f7a04a1e7c3bde
Gary Grumbley <ggrumbley@gmail.com>
2018-09-09 17:28:07 -0700

    Update basic react client app structure

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import Views from './views';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const App = <Views />;
+
+ReactDOM.render(App, document.getElementById('root'));
---
+++ client/src/views/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const App = () => (
+  <div className="App">
+    Hello!
+  </div>
+);
+
+
+export default App;
