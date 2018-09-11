a47e9990df8dde35c4b2afbd22a05ba6fb016ee8
Gary Grumbley <ggrumbley@gmail.com>
2018-09-11 13:35:41 -0700

    Add error messages to registration page

--- client/src/views/Register.js
+++ client/src/views/Register.js
@@ -1,37 +1,75 @@
 import React from 'react';
 import { graphql } from 'react-apollo';
 import { gql } from 'apollo-boost';
-import { Input, Container, Header, Button } from 'semantic-ui-react';
+import { Input, Container, Header, Button, Message } from 'semantic-ui-react';
 
 class Register extends React.Component {
   state = {
     username: '',
+    usernameError: '',
     email: '',
+    emailError: '',
     password: '',
+    passwordError: '',
   };
 
   onChange = e => {
     const { name, value } = e.target;
-    this.setState({ [name]: value });
+    this.setState({
+      [name]: value,
+    });
   };
 
   onSubmit = async () => {
-    const res = await this.props.mutate({
-      variables: this.state,
+    this.setState({
+      usernameError: '',
+      emailError: '',
+      passwordError: '',
     });
-    console.log(res);
+
+    const { username, email, password } = this.state;
+
+    const res = await this.props.mutate({
+      variables: { username, email, password },
+    });
+    const { ok, errors } = res.data.register;
+
+    if (ok) {
+      this.props.history.push('/');
+    } else {
+      const err = {};
+      errors.forEach(({ path, message }) => {
+        err[`${path}Error`] = message;
+      });
+
+      this.setState(err);
+    }
   };
 
   render() {
-    const { username, email, password } = this.state;
+    const { username, email, password, usernameError, emailError, passwordError } = this.state;
+
+    const errorList = [];
+
+    if (usernameError) {
+      errorList.push(usernameError);
+    }
+    if (emailError) {
+      errorList.push(emailError);
+    }
+    if (passwordError) {
+      errorList.push(passwordError);
+    }
+
     return (
       <Container text>
-        <Header as="h2">Register</Header>
+        <Header as="h2">Register</Header>{' '}
         <Input
           name="username"
           onChange={this.onChange}
           value={username}
           placeholder="Username"
+          error={!!usernameError}
           fluid
         />
         <Input
@@ -39,6 +77,7 @@ class Register extends React.Component {
           onChange={this.onChange}
           value={email}
           placeholder="Email"
+          error={!!emailError}
           fluid
         />
         <Input
@@ -47,9 +86,13 @@ class Register extends React.Component {
           type="password"
           value={password}
           placeholder="Password"
+          error={!!passwordError}
           fluid
         />
-        <Button onClick={this.onSubmit}>Submit</Button>
+        <Button onClick={this.onSubmit}> Submit </Button>
+        {usernameError || emailError || passwordError ? (
+          <Message error header="There was some errors with your submission" list={errorList} />
+        ) : null}
       </Container>
     );
   }
